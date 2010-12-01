1a5428602219e7936278c15f6138bce6299d0ea9
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2010-12-01 17:57:14 -0600

    Make Background windows on the two side panels.

--- szUI/frames.lua
+++ szUI/frames.lua
@@ -5,11 +5,11 @@ chat:SetFrameStrata("LOW")
 chat:SetPoint("BOTTOMLEFT", UIParent, .25*42, 0.25*42)
 chat:SetWidth(9.5*42)
 chat:SetHeight(4.25*42)
+MakeBkgWindow(chat)
 
 info = CreateFrame("FRAME", "pUI_InfoDock", UIParent)
 info:SetFrameStrata("LOW")
 info:SetPoint("BOTTOMRIGHT", UIParent, -10, 10)
 info:SetWidth(9.5*42)
 info:SetHeight(178)
-
-
+MakeBkgWindow(info)
